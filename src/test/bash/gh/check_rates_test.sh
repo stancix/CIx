11c575306234949f23873dbbbc630005109c09a8
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-30 20:56:07 +0300

    tests

--- src/test/bash/gh/check_rates_test.sh
+++ src/test/bash/gh/check_rates_test.sh
@@ -14,6 +14,16 @@ STDERR="$(mktemp)"
 
 #
 
+:> "${STDOUT}"
+:> "${STDERR}"
+CIX_SHARED="$(mktemp -d)"
+PATH="${mocks}/curl/bin:${PATH}" \
+ "${SCRIPT}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" $'Response error!\nGet limits error!\n'
+rm -r "${CIX_SHARED}"
+
 echo 'Not implemented!'; exit 1 # todo
 
 #
