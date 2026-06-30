a5fefbc614dc5976f09e24f6d69a3fccd2d8e640
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-30 17:04:25 +0300

    checkout

--- src/main/bash/gh/checkout.sh
+++ src/main/bash/gh/checkout.sh
@@ -1,8 +1,8 @@
 #!/usr/local/bin/bash
 
-. $checks/require.sh REP_OWNER REP_NAME SOURCE_COMMIT TARGET_BRANCH GITHUB_PAT
+. $checks/strings/require.sh VCS_REP_OWNER VCS_REP_NAME VCS_SOURCE_COMMIT VCS_TARGET_BRANCH GITHUB_PAT
 
-VCS_URL="https://${GITHUB_PAT}@github.com/${REP_OWNER}/${REP_NAME}.git"
+VCS_URL="https://${GITHUB_PAT}@github.com/${VCS_REP_OWNER}/${VCS_REP_NAME}.git"
 
 git init \
  || . $checks/fail.sh 'Git init error!'
@@ -10,11 +10,11 @@ git init \
 git remote add origin "${VCS_URL}" \
  || . $checks/fail.sh 'Git remotes error!'
 
-git fetch origin "${TARGET_BRANCH}" \
- || . $checks/fail.sh "Git fetch \"${TARGET_BRANCH}\" error!"
+git fetch origin "${VCS_TARGET_BRANCH}" \
+ || . $checks/fail.sh "Git fetch \"${VCS_TARGET_BRANCH}\" error!"
 
-git fetch origin "${SOURCE_COMMIT}" \
- || . $checks/fail.sh "Git fetch \"${SOURCE_COMMIT}\" error!"
+git fetch origin "${VCS_SOURCE_COMMIT}" \
+ || . $checks/fail.sh "Git fetch \"${VCS_SOURCE_COMMIT}\" error!"
 
-git switch "${TARGET_BRANCH}" \
- || . $checks/fail.sh "Git switch \"${TARGET_BRANCH}\" error!"
+git switch "${VCS_TARGET_BRANCH}" \
+ || . $checks/fail.sh "Git switch \"${VCS_TARGET_BRANCH}\" error!"
