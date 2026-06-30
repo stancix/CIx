80bcaba740daffbbc07118feee4dc2cb1e1cf27b
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-30 21:34:19 +0300

    bash assemble

---
+++ src/main/bash/bash/assemble.sh
@@ -0,0 +1,19 @@
+#!/usr/local/bin/bash
+
+SCRIPT='./assemble.sh'
+#. $checks/files/execs.sh "${SCRIPT}" # todo
+
+"${SCRIPT}" \
+ || . $checks/fail.sh 'Assemble error!'
+
+SUBJECT='./build/yml/metadata.yml'
+#. $checks/files/not_empty.sh "${SUBJECT}" # todo
+
+ACTUAL_VERSION="$(yq -Mer '.version' "${SUBJECT}" 2> /dev/null)" \
+ || . $checks/fail.sh 'Get version error!'
+
+ACTUAL_REP_NAME="$(yq -Mer '.repository.name' "${SUBJECT}" 2> /dev/null)" \
+ || . $checks/fail.sh 'Get repository name error!'
+
+SUBJECT="./build/zip/${ACTUAL_REP_NAME}-${ACTUAL_VERSION}.zip"
+#. $checks/files/not_empty.sh "${SUBJECT}" # todo
