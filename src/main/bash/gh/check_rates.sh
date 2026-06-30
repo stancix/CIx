909ac40bddb82efa58e5b141201c9a1b3858dde2
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-30 21:07:10 +0300

    tests

--- src/main/bash/gh/check_rates.sh
+++ src/main/bash/gh/check_rates.sh
@@ -5,10 +5,10 @@ SUBJECT="${CIX_SHARED}/gh_limits.json"
 $ghx/rate_limit.sh "${SUBJECT}" \
  || . $checks/fail.sh 'Get limits error!'
 
-CIX_LIMIT="$(yq -Me -p=json -o=json '.resources.core.limit' "${SUBJECT}")" \
+CIX_LIMIT="$(yq -Me -p=json -o=json '.resources.core.limit' "${SUBJECT}" 2> /dev/null)" \
  || . $checks/fail.sh 'Get limit error!'
 
-CIX_REMAINING="$(yq -Me -p=json -o=json '.resources.core.remaining' "${SUBJECT}")" \
+CIX_REMAINING="$(yq -Me -p=json -o=json '.resources.core.remaining' "${SUBJECT}" 2> /dev/null)" \
  || . $checks/fail.sh 'Get remaining error!'
 
 . $checks/ints/gt.sh "${CIX_REMAINING}" 30 "Remaining ${CIX_REMAINING}/${CIX_LIMIT}!"
