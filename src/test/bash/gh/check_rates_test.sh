6b8341128958b7869a330469dd18e92eecef8046
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-06-30 21:08:27 +0300

    tests

--- src/test/bash/gh/check_rates_test.sh
+++ src/test/bash/gh/check_rates_test.sh
@@ -19,6 +19,18 @@ STDERR="$(mktemp)"
 CIX_SHARED="$(mktemp -d)"
 PATH="${mocks}/curl/bin:${PATH}" \
  CIX_SHARED="${CIX_SHARED}" \
+ MOCKS_CURL_EXIT_CODE=1 \
+ "${SCRIPT}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/equals.sh "${STDERR}" $'Request error!\nGet limits error!\n'
+rm -r "${CIX_SHARED}"
+
+:> "${STDOUT}"
+:> "${STDERR}"
+CIX_SHARED="$(mktemp -d)"
+PATH="${mocks}/curl/bin:${PATH}" \
+ CIX_SHARED="${CIX_SHARED}" \
  MOCKS_CURL_HTTP_CODE=500 \
  "${SCRIPT}" > "${STDOUT}" 2> "${STDERR}"
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 1
