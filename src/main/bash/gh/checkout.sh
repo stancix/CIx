5fe10f9bfa32ef19e5751fd39c86388e70ed0960
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-07-01 17:34:17 +0300

    wip

--- src/main/bash/gh/checkout.sh
+++ src/main/bash/gh/checkout.sh
@@ -4,17 +4,17 @@
 
 VCS_URL="https://${GITHUB_WORKER_PAT}@github.com/${VCS_REP_OWNER}/${VCS_REP_NAME}.git"
 
-git init --quiet \
+git -С "${CIX_WORKDIR}" init --quiet \
  || . $checks/fail.sh 'Git init error!'
 
-git remote add origin "${VCS_URL}" \
+git -C "${CIX_WORKDIR}" remote add origin "${VCS_URL}" \
  || . $checks/fail.sh 'Git remotes error!'
 
-git fetch origin "${VCS_TARGET_BRANCH}" \
+git -C "${CIX_WORKDIR}" fetch origin "${VCS_TARGET_BRANCH}" \
  || . $checks/fail.sh "Git fetch \"${VCS_TARGET_BRANCH}\" error!"
 
-git fetch origin "${VCS_SOURCE_COMMIT}" \
+git -C "${CIX_WORKDIR}" fetch origin "${VCS_SOURCE_COMMIT}" \
  || . $checks/fail.sh "Git fetch \"${VCS_SOURCE_COMMIT}\" error!"
 
-git switch "${VCS_TARGET_BRANCH}" \
+git -C "${CIX_WORKDIR}" switch "${VCS_TARGET_BRANCH}" \
  || . $checks/fail.sh "Git switch \"${VCS_TARGET_BRANCH}\" error!"
