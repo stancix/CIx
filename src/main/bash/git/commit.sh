3b0ceb55a86da4953036679c97341a0eff5e2e64
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-07-01 18:31:31 +0300

    wip

--- src/main/bash/git/commit.sh
+++ src/main/bash/git/commit.sh
@@ -7,11 +7,11 @@ CIX_COMMIT_MESSAGE="$2"
 
 . $checks/strings/require.sh CIX_COMMIT_TAG CIX_COMMIT_MESSAGE
 
-git add . \
+git -C "${CIX_WORKDIR}" add . \
  || . $checks/fail.sh 'Git add error!'
 
-git commit -S -m "${CIX_COMMIT_MESSAGE}" \
+git -C "${CIX_WORKDIR}" commit -S -m "${CIX_COMMIT_MESSAGE}" \
  || . $checks/fail.sh 'Git sign commit error!'
 
-git tag -s "${CIX_COMMIT_TAG}" -m "${CIX_COMMIT_MESSAGE}" \
+git -C "${CIX_WORKDIR}" tag -s "${CIX_COMMIT_TAG}" -m "${CIX_COMMIT_MESSAGE}" \
  || . $checks/fail.sh 'Git sign tag error!'
