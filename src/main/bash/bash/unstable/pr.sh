6d899a03e481bbd9468d51206a57aa1076b9a28d
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-07-01 17:31:36 +0300

    wip

---
+++ src/main/bash/bash/unstable/pr.sh
@@ -0,0 +1,23 @@
+#!/usr/local/bin/bash
+
+. $cix/gh/check_rates.sh
+
+. $cix/gh/checkout.sh
+
+. $cix/gh/config.sh
+
+. $cix/git/merge.sh
+
+. $cix/bash/assemble.sh
+
+echo 'Not implemented!'; exit 1 # todo
+
+. $cix/bash/commit.sh
+
+echo 'Not implemented!'; exit 1 # todo
+
+# todo push
+
+# todo release
+
+# todo message
