2c0eaaf9317bd9c4fbfd2c34336a9bff6f794e57
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-08-19 23:34:25 +0300

    push

--- src/main/bash/gh/push.sh
+++ src/main/bash/gh/push.sh
@@ -1,20 +1,18 @@
 #!/usr/local/bin/bash
 
-SUBJECT='./build/yml/metadata.yml'
-. $checks/files/not_empty.sh "${SUBJECT}"
+. $checks/ints/eq.sh $# 2 'Wrong arguments!'
 
-ACTUAL_REP_OWNER="$(yq -Mer '.repository.owner' "${SUBJECT}" 2> /dev/null)" \
- || . $checks/fail.sh 'Get repository owner error!'
+CIX_REP_OWNER="$1"
+CIX_REP_NAME="$2"
 
-ACTUAL_REP_NAME="$(yq -Mer '.repository.name' "${SUBJECT}" 2> /dev/null)" \
- || . $checks/fail.sh 'Get repository name error!'
+. $checks/strings/require.sh CIX_REP_OWNER CIX_REP_NAME
 
-git push --follow-tags \
+git -C "${CIX_WORKDIR}" push --follow-tags \
  || . $checks/fail.sh 'Push error!'
 
-CIX_RESULT_COMMIT="$(git rev-parse HEAD)" \
+CIX_RESULT_COMMIT="$(git -C "${CIX_WORKDIR}" rev-parse HEAD)" \
  || . $checks/fail.sh 'Get commit SHA error!'
 
 SUBJECT="${CIX_SHARED}/gh_commit.json"
 
-. $ghx/commit.sh "${ACTUAL_REP_OWNER}" "${ACTUAL_REP_NAME}" "${CIX_RESULT_COMMIT}" "${SUBJECT}"
+. $ghx/commit.sh "${CIX_REP_OWNER}" "${CIX_REP_NAME}" "${CIX_RESULT_COMMIT}" "${SUBJECT}"
