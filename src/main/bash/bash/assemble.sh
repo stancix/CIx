2841092669271311b4d349077311cc392e9012aa
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-08-19 22:41:04 +0300

    tests

--- src/main/bash/bash/assemble.sh
+++ src/main/bash/bash/assemble.sh
@@ -8,13 +8,13 @@ BUILD_VARIANT="$3"
 
 . $checks/strings/require.sh CIX_REP_OWNER CIX_REP_NAME BUILD_VARIANT SIGNING_ALIAS
 
-SCRIPT='./assemble.sh'
+SCRIPT="${CIX_WORKDIR}/assemble.sh"
 . $checks/files/execs.sh "${SCRIPT}"
 
 "${SCRIPT}" "${BUILD_VARIANT}" \
  || . $checks/fail.sh 'Assemble error!'
 
-SUBJECT='./build/yml/metadata.yml'
+SUBJECT="${CIX_WORKDIR}/build/yml/metadata.yml"
 . $checks/files/not_empty.sh "${SUBJECT}"
 
 #
@@ -29,7 +29,7 @@ ACTUAL_REP_NAME="$(yq -Mer '.repository.name' "${SUBJECT}" 2> /dev/null)" \
  || . $checks/fail.sh 'Get repository name error!'
 
 ACTUAL_ALIAS="$(yq -Mer '.signing.alias' "${SUBJECT}" 2> /dev/null)" \
- || . $checks/fail.sh 'Get signing type error!'
+ || . $checks/fail.sh 'Get signing alias error!'
 
 . $checks/strings/eq.sh "${ACTUAL_VARIANT}" "${BUILD_VARIANT}"
 . $checks/strings/eq.sh "${ACTUAL_REP_OWNER}" "${CIX_REP_OWNER}"
@@ -39,5 +39,5 @@ ACTUAL_ALIAS="$(yq -Mer '.signing.alias' "${SUBJECT}" 2> /dev/null)" \
 BUILD_VERSION="$(yq -Mer '.build.version' "${SUBJECT}" 2> /dev/null)" \
  || . $checks/fail.sh 'Get version error!'
 
-SUBJECT="./build/zip/${CIX_REP_NAME}-${BUILD_VERSION}.zip"
+SUBJECT="${CIX_WORKDIR}/build/zip/${CIX_REP_NAME}-${BUILD_VERSION}.zip"
 . $checks/files/not_empty.sh "${SUBJECT}"
--- src/test/bash/bash/assemble_test.sh
+++ src/test/bash/bash/assemble_test.sh
@@ -14,7 +14,39 @@ STDERR="$(mktemp)"
 
 #
 
-echo 'Not implemented!'; exit 1 # todo
+TEST_REP_OWNER='testrepowner'
+TEST_REP_NAME='testrepname'
+TEST_VARIANT='testvariant'
+TEST_VERSION='testversion'
+TEST_ALIAS='testalias'
+
+CIX_WORKDIR="$(mktemp -d)"
+
+printf '%s' "\
+[[ '${TEST_VARIANT}' == \"\$1\" ]] || exit 1
+mkdir '${CIX_WORKDIR}/build/'
+mkdir '${CIX_WORKDIR}/build/yml/'
+printf '%s' '{
+ \"repository\":{\"owner\":\"${TEST_REP_OWNER}\",\"name\":\"${TEST_REP_NAME}\"},
+ \"build\":{\"variant\":\"${TEST_VARIANT}\",\"version\":\"${TEST_VERSION}\"},
+ \"signing\":{\"alias\":\"${TEST_ALIAS}\"}
+}' > '${CIX_WORKDIR}/build/yml/metadata.yml'
+mkdir '${CIX_WORKDIR}/build/zip/'
+printf '42' > '${CIX_WORKDIR}/build/zip/${TEST_REP_NAME}-${TEST_VERSION}.zip'
+" > "${CIX_WORKDIR}/assemble.sh"
+chmod +x "${CIX_WORKDIR}/assemble.sh"
+
+#
+
+:> "${STDOUT}"
+:> "${STDERR}"
+CIX_WORKDIR="${CIX_WORKDIR}" \
+ SIGNING_ALIAS="${TEST_ALIAS}" \
+ "${SCRIPT}" "${TEST_REP_OWNER}" "${TEST_REP_NAME}" "${TEST_VARIANT}" > "${STDOUT}" 2> "${STDERR}"
+. $asserts/ints/eq.sh "${SCRIPT}" "$?" 0
+. $asserts/files/empty.sh "${STDOUT}"
+. $asserts/files/empty.sh "${STDERR}"
+rm -rf "${CIX_WORKDIR}"
 
 #
 
