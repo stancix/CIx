d3fda768f74f8fb1b5b1147df1d19f16d5b2ed21
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-08-19 23:21:07 +0300

    commit

--- src/main/bash/bash/commit.sh
+++ src/main/bash/bash/commit.sh
@@ -6,13 +6,13 @@ CIX_REP_OWNER="$1"
 CIX_REP_NAME="$2"
 CIX_DST_BRANCH="$3"
 
+. $checks/strings/require.sh CIX_REP_OWNER CIX_REP_NAME CIX_DST_BRANCH
+
 SUBJECT="${CIX_WORKDIR}/build/yml/metadata.yml"
 . $checks/files/not_empty.sh "${SUBJECT}"
 
 BUILD_VERSION="$(yq -Mer '.build.version' "${SUBJECT}" 2> /dev/null)" \
  || . $checks/fail.sh 'Get version error!'
-
-. $checks/strings/require.sh CIX_REP_OWNER CIX_REP_NAME BUILD_VERSION CIX_DST_BRANCH
 
 . $ghx/refs/not_exists.sh "${CIX_REP_OWNER}" "${CIX_REP_NAME}" "tags/${BUILD_VERSION}"
 
