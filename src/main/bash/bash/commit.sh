4c4b77480b8498410b513326d923ab2dd6bb3b7a
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-08-19 23:15:19 +0300

    commit

--- src/main/bash/bash/commit.sh
+++ src/main/bash/bash/commit.sh
@@ -1,13 +1,19 @@
 #!/usr/local/bin/bash
 
+. $checks/ints/eq.sh $# 3 'Wrong arguments!'
+
+CIX_REP_OWNER="$1"
+CIX_REP_NAME="$2"
+CIX_DST_BRANCH="$2"
+
 SUBJECT="${CIX_WORKDIR}/build/yml/metadata.yml"
 . $checks/files/not_empty.sh "${SUBJECT}"
 
 BUILD_VERSION="$(yq -Mer '.build.version' "${SUBJECT}" 2> /dev/null)" \
  || . $checks/fail.sh 'Get version error!'
 
-. $checks/strings/require.sh VCS_REP_OWNER VCS_REP_NAME BUILD_VERSION VCS_DST_BRANCH
+. $checks/strings/require.sh CIX_REP_OWNER CIX_REP_NAME BUILD_VERSION CIX_DST_BRANCH
 
-. $ghx/refs/not_exists.sh "${VCS_REP_OWNER}" "${VCS_REP_NAME}" "tags/${BUILD_VERSION}"
+. $ghx/refs/not_exists.sh "${CIX_REP_OWNER}" "${CIX_REP_NAME}" "tags/${BUILD_VERSION}"
 
-. $cix/git/commit.sh "${BUILD_VERSION}" "${VCS_DST_BRANCH} <- ${BUILD_VERSION}"
+. $cix/git/commit.sh "${BUILD_VERSION}" "${CIX_DST_BRANCH} <- ${BUILD_VERSION}"
