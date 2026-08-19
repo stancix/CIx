e9fe91668751077d6452e542a92f99292ce65e75
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-08-19 22:14:00 +0300

    wip

--- src/main/bash/bash/assemble.sh
+++ src/main/bash/bash/assemble.sh
@@ -6,7 +6,7 @@ CIX_REP_OWNER="$1"
 CIX_REP_NAME="$2"
 BUILD_VARIANT="$3"
 
-. $checks/strings/require.sh CIX_REP_OWNER CIX_REP_NAME BUILD_VARIANT SIGNING_KEY_ALIAS
+. $checks/strings/require.sh CIX_REP_OWNER CIX_REP_NAME BUILD_VARIANT SIGNING_ALIAS
 
 SCRIPT='./assemble.sh'
 . $checks/files/execs.sh "${SCRIPT}"
@@ -17,13 +17,10 @@ SCRIPT='./assemble.sh'
 SUBJECT='./build/yml/metadata.yml'
 . $checks/files/not_empty.sh "${SUBJECT}"
 
+#
+
 ACTUAL_VARIANT="$(yq -Mer '.build.variant' "${SUBJECT}" 2> /dev/null)" \
  || . $checks/fail.sh 'Get variant error!'
-
-. $checks/strings/eq.sh "${ACTUAL_VARIANT}" "${BUILD_VARIANT}"
-
-BUILD_VERSION="$(yq -Mer '.build.version' "${SUBJECT}" 2> /dev/null)" \
- || . $checks/fail.sh 'Get version error!'
 
 ACTUAL_REP_OWNER="$(yq -Mer '.repository.owner' "${SUBJECT}" 2> /dev/null)" \
  || . $checks/fail.sh 'Get repository owner error!'
@@ -31,12 +28,16 @@ ACTUAL_REP_OWNER="$(yq -Mer '.repository.owner' "${SUBJECT}" 2> /dev/null)" \
 ACTUAL_REP_NAME="$(yq -Mer '.repository.name' "${SUBJECT}" 2> /dev/null)" \
  || . $checks/fail.sh 'Get repository name error!'
 
-SIGNING_TYPE="$(yq -Mer '.signing' "${SUBJECT}" 2> /dev/null)" \
+ACTUAL_ALIAS="$(yq -Mer '.signing.alias' "${SUBJECT}" 2> /dev/null)" \
  || . $checks/fail.sh 'Get signing type error!'
 
+. $checks/strings/eq.sh "${ACTUAL_VARIANT}" "${BUILD_VARIANT}"
 . $checks/strings/eq.sh "${ACTUAL_REP_OWNER}" "${CIX_REP_OWNER}"
 . $checks/strings/eq.sh "${ACTUAL_REP_NAME}" "${CIX_REP_NAME}"
-. $checks/strings/eq.sh "${SIGNING_TYPE}" "${SIGNING_KEY_ALIAS}"
+. $checks/strings/eq.sh "${ACTUAL_ALIAS}" "${SIGNING_ALIAS}"
+
+BUILD_VERSION="$(yq -Mer '.build.version' "${SUBJECT}" 2> /dev/null)" \
+ || . $checks/fail.sh 'Get version error!'
 
 SUBJECT="./build/zip/${CIX_REP_NAME}-${BUILD_VERSION}.zip"
 . $checks/files/not_empty.sh "${SUBJECT}"
