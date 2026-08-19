235677f0bd0ea12c7963b39c5745828ce54776aa
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-08-19 19:40:05 +0300

    tests

--- src/test/bash/gh/config_keys_test.sh
+++ src/test/bash/gh/config_keys_test.sh
@@ -15,23 +15,27 @@ STDERR="$(mktemp)"
 #
 
 CIX_WORKDIR="$(mktemp -d)"
+CIX_SHARED="$(mktemp -d)"
 
 git -C "${CIX_WORKDIR}" init --quiet \
  || . $asserts/fail.sh 'Git init error!'
 
 #
 
+cp 'src/test/res/key.gpg' "${CIX_SHARED}/key.gpg" || exit 1
+
 :> "${STDOUT}"
 :> "${STDERR}"
 CIX_WORKER_KEY_ID='2CFF9BD0725E548B'
-CIX_WORKER_KEY='src/test/res/key.pgp'
 CIX_WORKER_EMAIL='foo@bar.baz'
 CIX_WORKDIR="${CIX_WORKDIR}" \
- "${SCRIPT}" "${CIX_WORKER_KEY_ID}" "${CIX_WORKER_KEY}" "${CIX_WORKER_EMAIL}" > "${STDOUT}" 2> "${STDERR}"
+CIX_SHARED="${CIX_SHARED}" \
+ "${SCRIPT}" "${CIX_WORKER_KEY_ID}" "${CIX_WORKER_EMAIL}" > "${STDOUT}" 2> "${STDERR}"
 . $asserts/ints/eq.sh "${SCRIPT}" "$?" 0
 . $asserts/files/empty.sh "${STDOUT}"
 . $asserts/files/empty.sh "${STDERR}"
 rm -rf "${CIX_WORKDIR}"
+rm -rf "${CIX_SHARED}"
 
 #
 
