ac23b8c36de547724071afc89e4a1de903a6f442
Stanley Wintergreen <6218886+kepocnhh@users.noreply.github.com>
2026-08-21 15:16:57 +0300

    untracked

--- src/main/bash/git/commit.sh
+++ src/main/bash/git/commit.sh
@@ -9,6 +9,8 @@ CIX_COMMIT_MESSAGE="$2"
 
 . $checks/strings/eq.sh "$(git -C "${CIX_WORKDIR}" diff --name-only)" '' 'Git unstaged error!'
 
+. $checks/strings/eq.sh "$(git -C "${CIX_WORKDIR}" ls-files --others --exclude-standard)" '' 'Git untracked error!'
+
 git -C "${CIX_WORKDIR}" commit -S -m "${CIX_COMMIT_MESSAGE}" \
  || . $checks/fail.sh 'Git sign commit error!'
 
